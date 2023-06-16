f537fbb53c609c33ac3c9920f685d7a2b1c3f08e
Komron <komronabdujabbor@gmail.com>
2023-06-16 19:32:36 +0500

    ToString methodga override yozildi

--- kutubxona/Book.cs
+++ kutubxona/Book.cs
@@ -10,16 +10,24 @@ namespace Library_management
     {
         public Guid ID { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
+        
         public string Author { get; set; }
         public string Category { get; set; }
         public Guid? ReaderID { get; set; }
-        public Book(string name, string description, string author)
+        public Book(string name, string author)
         {
             ID= Guid.NewGuid();
             Name=name;
-            Description=description;
             Author = author;
+            //ReaderID=Guid.NewGuid();
+        }
+        public override string ToString()
+        {
+            return $"Kitob" +
+                $"\nid: {ID}"+
+                $"\nnomi: {Name}"+
+                $"\nMuallif: {Author}" +
+                $"\nKim o'qiyapti {ReaderID}";
         }
     }
 }
