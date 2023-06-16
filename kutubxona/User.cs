a7735dda61b5c35b72410ac28f6a23e461867a15
Komron <komronabdujabbor@gmail.com>
2023-06-16 19:33:22 +0500

    ToString metodiga override yozildi Id to'g'irlandi

--- kutubxona/User.cs
+++ kutubxona/User.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Library_management
 {
     public class User
     {
-        public Guid ID { get; set; }
+        public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
 
         public User(string firstName, string lastName)
         {
-            ID = Guid.NewGuid();
+            Id = Guid.NewGuid();
             FirstName = firstName;
             LastName = lastName;
         }
         public User(string firstName, string lastName, string username):this(firstName, lastName)
         {
             UserName= username;
+        }
+        public override string ToString()
+        {
+            return $"Kitob" +
+            $"\nid: {Id}" +
+                $"\nFirstName: {FirstName}" +
+                $"\nNLastName: {LastName}" +
+                $"\nUserName: {UserName}";
         }
     }
 }
