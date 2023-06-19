431930cf7ec758fd00048ce587850645160c9a48
Komron <komronabdujabbor@gmail.com>
2023-06-19 18:53:02 +0500

    core class abstractga o'zgargani uchun o'chirib tashladim

---
+++ kutubxona/LibraryManagement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_management
+{
+    public class LibraryManagement : lib_management_core
+    {
+        public void RegisterUser(string firstName, string lastName)
+        {
+            
+        }
+        public void RegisterUser(string firstName, string lastName, string username)
+        {
+            var founduser = false;
+            foreach (var user in Store.Users)
+            {
+                if (user.UserName == username)
+                {
+                    founduser = true;
+                    break;
+                }
+            }
+            if (founduser)
+            {
+                return;
+            }
+            Store.Users.Add(new User(firstName, lastName, username));
+        }
+    }
+}
--- kutubxona/Program.cs
+++ kutubxona/Program.cs
@@ -1,5 +1,17 @@
 ﻿using Library_management;
+//while (true)
+//{
+//    Console.WriteLine("Enter command" +
+//        "\nRegister User - 1" +
+//        "\nRemove User - 2" +
+//        "\nAdd book - 3" +
+//        "\nRemove book - 4" +
+//        "\nAll books - 5" +
+//        "\nFind books - 6" +
+//        "\nMost book-a-holic - 7" +
+//        "\nAvailable books - 8\n");
+
+//    var command = Console.ReadLine();
+//}
+
 using LibraryManagement;
-var libManage = new lib_management_core(DataSource.Books, DataSource.Users);
-libManage.DisplayAllBooks();
-libManage.DisplayAllUsers();
