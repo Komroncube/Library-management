a5279464252f3500b03160d94eebb23bd94683bd
Komron <komronabdujabbor@gmail.com>
2023-06-29 13:19:04 +0500

    class renamed

---
+++ kutubxona/EmailMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_management
+{
+    public class EmailMessage
+    {
+        public string From { get; set; } = string.Empty;
+        public string To { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
