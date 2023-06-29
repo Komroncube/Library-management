51d7c2c4ab87b12d62e6bba326c27f7f9393f20c
Komron <komronabdujabbor@gmail.com>
2023-06-29 13:21:39 +0500

    email  shabloni yaratildi lekin kodda ishlatilmadi

---
+++ kutubxona/EmailTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_management
+{
+    public class EmailTemplate
+    {
+        public string Title { get; set; }= string.Empty;
+        public string Body { get; set; } = string.Empty;
+    }
+}
